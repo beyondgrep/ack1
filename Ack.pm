6a82713ab6f3188a5990d815a99df24ae57396e7
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2006-08-25 01:29:24 +0000

    Releasing 1.26

--- Ack.pm
+++ Ack.pm
@@ -9,11 +9,11 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 1.25_03
+Version 1.26
 
 =cut
 
-our $VERSION = '1.25_03';
+our $VERSION = '1.26';
 
 =head1 SYNOPSIS
 

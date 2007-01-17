0558bb0cd2fb70d4e9bc05ff215b65ade6d25b6a
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2007-01-17 18:56:47 +0000

    Updating to 1.56

--- Ack.pm
+++ Ack.pm
@@ -9,13 +9,13 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 1.54
+Version 1.56
 
 =cut
 
 our $VERSION;
 BEGIN {
-    $VERSION = '1.54';
+    $VERSION = '1.56';
 }
 
 our %types;

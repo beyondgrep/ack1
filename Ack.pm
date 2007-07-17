0922c0663df261478f0922c9ea17bd2447e6fb62
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2007-07-17 15:53:33 +0000

    Releasing 1.64

--- Ack.pm
+++ Ack.pm
@@ -9,13 +9,13 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 1.63_04
+Version 1.64
 
 =cut
 
 our $VERSION;
 BEGIN {
-    $VERSION = '1.63_04';
+    $VERSION = '1.64';
 }
 
 our %types;

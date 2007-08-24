4f8ec1d44aef68551348f941865c6d690b5df7a9
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2007-08-24 01:15:02 +0000

    make sets_match wrap lists_match

--- t/Util.pm
+++ t/Util.pm
@@ -50,20 +50,8 @@ sub sets_match {
     my @expected = @{+shift};
     my $msg = shift;
 
-    # Normalize all the paths
-    for my $path ( @expected, @actual ) {
-        $path = File::Next::reslash( $path ); ## no critic (Variables::ProhibitPackageVars)
-    }
-
     local $Test::Builder::Level = $Test::Builder::Level + 1; ## no critic
-
-    eval 'use Test::Differences';
-    if ( !$@ ) {
-        return eq_or_diff( [sort @actual], [sort @expected], $msg );
-    }
-    else {
-        return is_deeply( [sort @actual], [sort @expected], $msg );
-    }
+    return lists_match( [sort @actual], [sort @expected], $msg );
 }
 
 sub is_filetype {

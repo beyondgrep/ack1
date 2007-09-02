37ede75f57c0f69846da992567408d95813ebcd3
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2007-09-02 17:15:55 +0000

    up to 1.67_01

--- Ack.pm
+++ Ack.pm
@@ -9,14 +9,14 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 1.66
+Version 1.67_01
 
 =cut
 
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    $VERSION = '1.66';
+    $VERSION = '1.67_01';
     $COPYRIGHT = 'Copyright 2005-2007 Andy Lester, all rights reserved.';
 }
 

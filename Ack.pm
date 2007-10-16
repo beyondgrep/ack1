de0bcbfc622919084b233191b0a54ec8ceec845c
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2007-10-16 20:02:15 +0000

    releasing 1.68

--- Ack.pm
+++ Ack.pm
@@ -9,14 +9,14 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 1.67_02
+Version 1.68
 
 =cut
 
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    $VERSION = '1.67_02';
+    $VERSION = '1.68';
     $COPYRIGHT = 'Copyright 2005-2007 Andy Lester, all rights reserved.';
 }
 
--- ack
+++ ack
@@ -3,7 +3,7 @@
 use warnings;
 use strict;
 
-our $VERSION   = '1.67_02';
+our $VERSION   = '1.68';
 # Check http://petdance.com/ack/ for updates
 
 # These are all our globals.

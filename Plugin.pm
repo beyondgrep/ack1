750b9e29feb71d4358b5d7b91f64db7856ce37bb
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2008-07-10 03:55:48 +0000

    braindumping ideas

--- Plugin.pm
+++ Plugin.pm
@@ -6,22 +6,53 @@ App::Ack::Plugin - Basic plugin documentation for ack
 
 =head1 SYNOPSIS
 
-This module depines and demonstrates how ack plugins work.
+This module defines and demonstrates how ack plugins work.
+
+=head1 PLUGIN USES
+
+This is designed as a framework to let the plugins do all the work.  Here are some ideas.
+
+=over 4
+
+=item .xls
+
+Iterate through sheets within the workbook.
+
+=item .doc
+
+Search through text of the Word document.
+
+=back
+
+=head1 TERMS
+
+A resource is...
+
+A line is...
 
 =head1 FUNCTIONS
 
 Each App::Ack::Plugin module must include the following functions,
 with the specified functionality.
 
+This is how, roughly, the 
+
+    my $plugin = App::Ack::Plugin::Example->new( $filename );
+
+    while ( my $resource = $plugin->next_resource() ) {
+        # Handle start of the resource
+        while ( my $line = $plugin->next_line() ) {
+            # Search for stuff in $line
+        }
+        $resource->close_resource();
+    }
+    $plugin->shutdown();
+
 =head2 new( $filename )
 
 =cut
 
 =head2 next_resource()
-
-=cut
-
-=head2 first_line( $filename )
 
 =cut
 

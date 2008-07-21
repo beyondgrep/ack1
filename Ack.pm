4be8b099cce4d3a4e00a9d9d307cfbbce89c3c71
petdance <petdance@0c8536a8-3c1a-0410-a247-91dcf638db7e>
2008-07-21 23:39:01 +0000

    first stab at tar

--- Ack.pm
+++ Ack.pm
@@ -1258,7 +1258,14 @@ sub print_matches {
 
     my $nmatches = 0;
     while ( defined ( my $filename = $iter->() ) ) {
-        my $repo = App::Ack::Repository->new( $filename ) or next;
+        my $repo;
+        if ( $filename =~ /\.tar\.gz$/ ) {
+            $repo = App::Ack::Repository::Tar->new( $filename );
+        }
+        else {
+            $repo = App::Ack::Repository->new( $filename );
+        }
+        $repo or next;
 
         while ( my $res = $repo->next_resource() ) {
             my $needs_line_scan;
@@ -1446,5 +1453,96 @@ This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
+
+use Archive::Tar;
+
+package App::Ack::Repository::Tar;
+
+sub new {
+    my $class = shift;
+    my $filename = shift;
+
+    my $self = bless {
+        filename => $filename,
+    }, $class;
+
+    my $tar = Archive::Tar->new( $filename );
+    if ( !$tar ) {
+        return;
+    }
+
+    $self->{tar} = $tar;
+    $self->{files} = [$tar->get_files];
+
+    return $self;
+}
+
+sub next_resource {
+    my $self = shift;
+
+    my $file = shift @{$self->{files}};
+
+    return unless $file;
+
+    my $tar = $self->{tar};
+    my $res = App::Ack::Resource::Tar->new( $self->{filename}, $file->name, $file->get_content );
+
+    return $res;
+}
+
+sub close {
+}
+
+package App::Ack::Resource::Tar;
+
+sub new {
+    my $class    = shift;
+    my $tarname  = shift;
+    my $filename = shift;
+    my $content  = shift;
+
+    my $self = bless {
+        tarname  => $tarname,
+        filename => $filename,
+        lines    => [split( /\n/, $content )],
+        lineno   => 0,
+    }, $class;
+
+    return $self;
+}
+
+sub is_binary {
+    return;
+}
+
+sub name {
+    my $self = shift;
+
+    return $self->{filename};
+}
+
+sub needs_line_scan {
+    1; # XXX Do actual looking
+}
+
+sub reset {
+}
+
+sub next_text {
+    my $self = shift;
+
+    $_ = shift @{$self->{lines}};
+    if ( defined $_ ) {
+        $. = $self->{lineno}++;
+        return 1;
+    }
+
+    return 0;
+}
+
+sub close {
+}
+
+
 
 1; # End of App::Ack

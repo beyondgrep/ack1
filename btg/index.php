647b2502f58a0a046ca3849ea177cfd2de98b3cd
Andy Lester <andy@petdance.com>
2010-06-25 15:29:27 -0500

    updated the Google tracker

--- btg/index.php
+++ btg/index.php
@@ -233,15 +233,19 @@ $ ack --help-types
 </pre>
 
 
-        <!-- Google tracking, must be just before close of body tag -->
+<!-- Google tracking, must be just before close of body tag -->
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-631706-7");
-pageTracker._initData();
-pageTracker._trackPageview();
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-631706-7']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script');
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 
+'http://www') + '.google-analytics.com/ga.js';
+ga.setAttribute('async', 'true');
+document.documentElement.firstChild.appendChild(ga);
+})();
 </script>
     </body>
 </html>

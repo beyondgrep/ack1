74c0faa4c0779ddfb565fb1e9a0cffe0106ed9f1
Andy Lester <andy@petdance.com>
2010-09-05 14:17:03 -0500

    removed trailing whitespace

--- btg/index.php
+++ btg/index.php
@@ -241,7 +241,7 @@ _gaq.push(['_trackPageview']);
 
 (function() {
 var ga = document.createElement('script');
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
 'http://www') + '.google-analytics.com/ga.js';
 ga.setAttribute('async', 'true');
 document.documentElement.firstChild.appendChild(ga);
